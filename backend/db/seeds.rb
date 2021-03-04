dec101301c5a9478fd29d975fc52ec0f0b616d68
alan-mak <alanmak95@gmail.com>
2021-03-04 00:56:26 -0500

    Created database and seeded with working api server

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'SEEDING'
+Contact.create(name:'Alan', email:'a@b', message:'hi')
+puts 'DATABASE SEEDED'
