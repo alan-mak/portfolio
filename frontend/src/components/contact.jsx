e8bcbe58d8b3666d33e753877b4ca3832fbc1aa9
alan-mak <alanmak95@gmail.com>
2021-03-14 23:57:31 -0400

    Responsive contact

--- frontend/src/components/contact.jsx
+++ frontend/src/components/contact.jsx
@@ -1,58 +1,37 @@
 import React from 'react';
-import useApplicationData from '../hooks/useApplicationData';
 
 import './contact.scss';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import Gitdark from '../img/github-dark.png'
+import Linkedin from '../img/linkedin.png'
+import Email from '../img/email.png'
+import Twitter from '../img/twitter.png'
+import Gitdark from '../img/github.png'
 
 export default function Contact() {
-  const { handleInput } = useApplicationData();
+
   return (
     <div id="contact">
-      Feel free to contact me!
-      <div className="socials">
-        <a href="https://twitter.com/alan_mak13">
-          <FontAwesomeIcon icon={faTwitter} size="4x" id="twitter"/>
-        </a>
-        <a href="https://www.linkedin.com/in/alanmak95/">
-          <FontAwesomeIcon icon={faLinkedin} size="4x" id="linkedin"/>
-        </a>
-        <a href="https://www.instagram.com/alan_mak13/">
-          <FontAwesomeIcon icon={faInstagram} size="4x" id="instagram"/>
-        </a>
-        <a href="https://www.github.com/alan-mak">
-          <img src={Gitdark} alt="git dark" id="github"/>
-        </a>
-      </div>
-    <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
-      <label>Name
-        <input
-          type="text"
-          name="name"
-          onChange = {handleInput}
-        />
-      </label>
-      <label>Email
-        <input 
-          type="email"
-          name="email"
-          onChange = {handleInput}
-        />
-      </label>
-      <label>Message
-        <input
-          type="text"
-          name="message"
-          onChange = {handleInput}
-          id="messageBox"
-        />
-      </label>
-      <button onClick={() => console.log("BEEP")}>
-        Submit
-      </button>
-    </form>
+      <h1 id="contactHead">CONTACT</h1>
+      <div className="contactContainer">
+        <h1>Thanks For Visiting!<br />Get in touch</h1>
+        <div className="socials">
+          <div>
+            <img src={Twitter} alt="Twitter"
+              onClick={() => window.open("https://www.twitter.com/alan_mak13")} />
+            <img src={Linkedin} alt="Linkedin" onClick={() => {
+              window.open("https://www.linkedin.com/in/alanmak95/")
+            }}/>
+          </div>
+          <div>
+            <a href="mailto: mak_alan@icloud.com">
+              <img src={Email} alt="email"/>
+            </a>
+            <img src={Gitdark} alt="Github" onClick={() => {
+              window.open("https://www.github.com/alan-mak")
+            }}/>
+          </div>
+        </div>
+      </ div>
     </div>
   )
 }
