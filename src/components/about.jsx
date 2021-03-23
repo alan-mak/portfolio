bd5b83ea41a6e5ca20f8ad6a7a16d84b5199f138
alan-mak <alanmak95@gmail.com>
2021-03-23 00:06:48 -0400

    About me font change

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -9,7 +9,7 @@ export default function About(props) {
       <h1>ABOUT ME</h1>
       <div className="information">
         <img src={Alan} alt="Profile Pic"/>
-        <p>I am a Toronto based web developer with an interest in front end design. I studied at Ryerson University in Toronto for my Bachelors and Masters of Engineering in Aerospace Engineering. I then went to Lighthouse Lab's Web Development Bootcamp in 2020. <br/><br/>
+        <p><h3 className="title">Alan Mak</h3>I am a Toronto based web developer with an interest in front end design. I studied at Ryerson University in Toronto for my Bachelors and Masters of Engineering in Aerospace Engineering. I then went to Lighthouse Lab's Web Development Bootcamp in 2020. <br/><br/>
         <div><i>Checkout my projects </i><a href="/projects">HERE</a></div></p>
       </div>
     </div>
