c64cef34941dd9b2a0477285f1a38f829df264b2
alan-mak <alanmak95@gmail.com>
2021-03-22 22:53:30 -0400

    Landing page reformat

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -9,7 +9,7 @@ export default function Landing () {
     <div id="landing">
       <img src={Toronto} alt="background" />
       <div id="first">
-        <h1><strong>Hello my name is Alan</strong></h1>
+        <h1><strong>Hello my name is </strong><strong style={{color:"#2E8DB8"}}>Alan</strong></h1>
       </div>
       <div id="second">
         <h1><strong>Full-Stack Web Developer</strong></h1>
