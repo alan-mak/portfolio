07f74a8112a5db6817ff9eb10faa579c2c02c4d4
alan-mak <alanmak95@gmail.com>
2021-03-22 20:56:53 -0400

    Remove comments

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -83,50 +83,6 @@ export default function Skills(props) {
           </div>
         </div>
       </div>
-      {/*
-      <h1 id="skillTitle">Skills</h1>
-      <div className="language">
-        <h1>Languages</h1>
-        <div className="icon">
-          <img src={Javascript} alt="javascript" />
-          <img src={Ruby} alt="ruby" />
-          <img src={Matlab} alt="matlab" />
-          <img src={Python} alt="python" />
-        </div>
-      </div>
-      <div className="frontend">
-        <h1>Web Frontend</h1>
-        <div className="icon">
-          <img src={Reactpic} alt="react" />
-          <img src={HTMLpic} alt="html" />
-          <img src={Jquery} alt="jQuery" />
-          <img src={Sass} alt="sass" />
-          <img src={CSS} alt="css" />
-        </div>
-      </div>
-      <div className="backend">
-        <h1>Web Backend</h1>
-        <div className="icon">
-          <img src={Express} alt="express" />
-          <img src={Rails} alt="ruby on rails" />
-          <img src={NodeJS} alt="nodeJS" />
-        </div>
-      </div>
-      <div className="database">
-        <h1>Databases</h1>
-        <div className="icon">
-          <img src={Postgresql} alt="postgresql" />
-          <img src={MongoDB} alt="mongoDB" />
-        </div>
-      </div>
-      <div className="scm">
-        <h1>Source Code Management </h1>
-        <div className="icon">
-          <img src={Octocat} alt="octocat" />
-          <img src={Git} alt="git" />
-        </div>
-      </div>
-    */}
     </div>
   )
 }
