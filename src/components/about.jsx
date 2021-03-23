09dbe375442f278cc27788f4e135455c965d02bd
alan-mak <alanmak95@gmail.com>
2021-03-22 21:55:15 -0400

    Changed about me projects

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -9,7 +9,8 @@ export default function About(props) {
       <h1>ABOUT ME</h1>
       <div className="information">
         <img src={Alan} alt="Profile Pic"/>
-        <p>I am a Toronto based web developer with an interest in front end design. I studied at Ryerson University in Toronto for my Bachelors and Masters of Engineering in Aerospace Engineering. I then went to Lighthouse Lab's Web Development Bootcamp in 2020. You can check out some of the work I have done in the <a href="/projects">Projects</a> section</p>
+        <p>I am a Toronto based web developer with an interest in front end design. I studied at Ryerson University in Toronto for my Bachelors and Masters of Engineering in Aerospace Engineering. I then went to Lighthouse Lab's Web Development Bootcamp in 2020. <br/><br/>
+        <div><i>Checkout my projects </i><a href="/projects">HERE</a></div></p>
       </div>
     </div>
   )
